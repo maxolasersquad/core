fad00910509d78fddd450c1986fca4c8e495d698
Thomas Müller <thomas.mueller@tmit.eu>
2016-03-10 15:49:15 +0100

    Kill the old ftp storage implementation - refs #19941

--- apps/files_external/appinfo/app.php
+++ apps/files_external/appinfo/app.php
@@ -27,7 +27,6 @@
  */
 
 OC::$CLASSPATH['OC\Files\Storage\StreamWrapper'] = 'files_external/lib/streamwrapper.php';
-OC::$CLASSPATH['OC\Files\Storage\FTP'] = 'files_external/lib/ftp.php';
 OC::$CLASSPATH['OC\Files\Storage\OwnCloud'] = 'files_external/lib/owncloud.php';
 OC::$CLASSPATH['OC\Files\Storage\Google'] = 'files_external/lib/google.php';
 OC::$CLASSPATH['OC\Files\Storage\Swift'] = 'files_external/lib/swift.php';
